185c0b339306cb8d7ee60ddac4cd35d584093849
Pascal Hebbeker <pascal.hebbeker@natur.cuni.cz>
2019-05-10 16:43:59 +0200

    adding root function to mean/error calculation

--- analysis/statistics.py
+++ analysis/statistics.py
@@ -1,8 +1,10 @@
 """ Collection of Functions to perform and analyse correlations"""
 
-def mean_std_err(data_array):
+def mean_std_err(data_array, root = 1.0):
     """caculate the mean and the error of a quantity"""
     import numpy as np
     mean = np.mean(data_array)
     error = np.std(data_array, ddof = 1) / np.sqrt(len(data_array))
-    return mean, error
+    mean_root = np.power(mean, 1.0/root)
+    error_root = 1.0/root * error * np.power(mean, 1.0/root - 1.0)
+    return mean_root, error_root
